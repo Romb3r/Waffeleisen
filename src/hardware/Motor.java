91a830db9b0bd664ebbf6c406474826b1bc3c5f7
SG3521 <goedeckesoeren@googlemail.com>
2025-12-08 08:39:58 +0100

    waffelstate

--- src/hardware/Motor.java
+++ src/hardware/Motor.java
@@ -2,11 +2,13 @@ package hardware;
 
 import lejos.hardware.ev3.LocalEV3;
 import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.hardware.motor.EV3MediumRegulatedMotor;
 
 public class Motor {
 	// private Attribute
 	private EV3LargeRegulatedMotor CMotorLeft = new EV3LargeRegulatedMotor(LocalEV3.get().getPort("A"));			// EV3LareRegulatedMotor Objekt an Port A erzeugen
-	private EV3LargeRegulatedMotor CMotorRight = new EV3LargeRegulatedMotor(LocalEV3.get().getPort("B"));           // EV3LareRegulatedMotor Objekt an Port A erzeugen
+	private EV3LargeRegulatedMotor CMotorRight = new EV3LargeRegulatedMotor(LocalEV3.get().getPort("B"));           // EV3LareRegulatedMotor Objekt an Port B erzeugen
+	private EV3MediumRegulatedMotor CMotorState = new EV3MediumRegulatedMotor(LocalEV3.get().getPort("C"));			// EV3MediumRegulateMotor Objekt an Port C erzeugen
 	private int iSpeed;																								// Drehgeschwindigkeit der Motoren
 	private int iMaxAngle;																							// Drehwinkel der Motoren
 	
@@ -16,12 +18,13 @@ public class Motor {
 		this.iMaxAngle = iMaxAngle;																					//	 Initialisieren der Werte im Konstruktor
 		CMotorLeft.setSpeed(this.iSpeed);																			//  /
 		CMotorRight.setSpeed(this.iSpeed);																			// /
+		CMotorState.setSpeed(15);
 	}
 	
-	// öffentliche Methoden
+	// oeffentliche Methoden
 	public void vOpen() {
 	/*
-	 * Steuert den Motor so an, dass das Waffeleisen geöffnet wird
+	 * Steuert den Motor so an, dass das Waffeleisen geoeffnet wird
 	 */
 		CMotorLeft.forward();
 		CMotorRight.forward();
@@ -35,11 +38,29 @@ public class Motor {
 		CMotorRight.backward();
 	}
 	
+	public void vEinfahren() {
+	/*
+	 * Sensor fÃ¤hrt ins Werkzeug ein, zum WaffelStateCheck
+	 */
+		CMotorState.rotate(30);
+	}
+	
+	public void vAusfahren() {
+	/*
+	 * Sensor fÃ¤hrt aus dem Werkzeug heraus, auf Grundstellung
+	 */
+		CMotorState.rotate(-1*30);
+	}
+	
 	public EV3LargeRegulatedMotor getLeftMotor() {
 		return CMotorLeft;
 	}
 	
 	public EV3LargeRegulatedMotor getRightMotor() {
 		return CMotorRight;
+	}
+	
+	public EV3MediumRegulatedMotor getStateMotor() {
+		return CMotorState;
 	}
 }
--- src/modes/AutoMode.java
+++ src/modes/AutoMode.java
@@ -35,24 +35,24 @@ public class AutoMode extends Mode {
 		int iWaffleState = Define_WaffleState.iEmpty;
 		boolean boRun = true;
 		System.out.println("Start Knopf pressen wenn Teig im Eisen");
-		/*if(base_btn.boButtonPressedBlocking(Button.ID_ENTER)) {																	// Starte die Automatik Routine wenn Start Knopf gedrückt
-			while (boRun) {																										// Lasse solange den Automatikmodus laufen, bis der Stop Knopf gedrückt wurde
+		if(base_btn.boButtonPressedBlocking(Button.ID_ENTER)) {																	// Starte die Automatik Routine wenn Start Knopf gedrï¿½ckt
+			while (boRun) {																										// Lasse solange den Automatikmodus laufen, bis der Stop Knopf gedrï¿½ckt wurde
 				if (this.color_sensor.iEvalWaffleState() == Define_WaffleState.iEmpty) {
-					vWaitFor(Define_Timer.iFillUpTime); 																		// Wenn Waffeleisen kein Teig enthält, warte um Teig einfüllen zu können
+					vWaitFor(Define_Timer.iFillUpTime); 																		// Wenn Waffeleisen kein Teig enthï¿½lt, warte um Teig einfï¿½llen zu kï¿½nnen
 				}
 				this.motor.vClose();
 				while (iWaffleState == Define_WaffleState.iNotReady && iWaffleState != Define_WaffleState.iEmpty) {				// Laufe solange bis Waffel fertig
 					vCook(Define_Timer.iSleepTimeMS);																			// Waffeleisen zu: Backe Teig	
-					this.motor.vOpen();																							// Öffne Waffeleisen
-					//iWaffleState = this.color_sensor.iEvalWaffleState();																// Werte aus, ob Teig fertig, wenn ja gehe aus der Schleife raus
-					this.motor.vClose(); 																						// Schließe Waffeleisen um weiter zu backen
+					this.motor.vOpen();																							// ï¿½ffne Waffeleisen
+					iWaffleState = this.color_sensor.iEvalWaffleState();																// Werte aus, ob Teig fertig, wenn ja gehe aus der Schleife raus
+					this.motor.vClose(); 																						// Schlieï¿½e Waffeleisen um weiter zu backen
 				}
 				this.speaker.vDoBeep();																							// Waffel fertig, gebe einen Ton wieder
-				vWaitFor(Define_Timer.iWaffleRemovalTime);  																	// Warte um Waffel entnehmen zu können
-				boRun = !this.base_btn.boButtonPressedBlockingTimeout(Button.ID_ESCAPE, Define_Timer.iWaitTimeStopBtnPress);	// Wenn Stop Knopf gedrückt wurde, liefert die Funktion true zurück, allerdings soll dann der Automatikmodus abgebrochen werden, also invertieren mit "!"
-				Define_Timer.vResetSleepTime();																					// Setze Timer der Backzeit zurück für neue Waffel
+				vWaitFor(Define_Timer.iWaffleRemovalTime);  																	// Warte um Waffel entnehmen zu kï¿½nnen
+				boRun = !this.base_btn.boButtonPressedBlockingTimeout(Button.ID_ESCAPE, Define_Timer.iWaitTimeStopBtnPress);	// Wenn Stop Knopf gedrï¿½ckt wurde, liefert die Funktion true zurï¿½ck, allerdings soll dann der Automatikmodus abgebrochen werden, also invertieren mit "!"
+				Define_Timer.vResetSleepTime();																					// Setze Timer der Backzeit zurï¿½ck fï¿½r neue Waffel
 			}
-		}*/
+		}
 	}
 	
 	// statische Funktionen
@@ -61,12 +61,12 @@ public class AutoMode extends Mode {
 		 * Warte um die Zeit iWaitTime und dekrementiere um 5 Sekunden pro Aufruf in der Schleife
 		 */
 		Delay.msDelay(iWaitTime);																						// Warte um die Zeit iWaitTime
-		Define_Timer.iSleepTimeMS -= 5 * 1000;																			// Dekrementiere die Zeit für die die Waffel weiter gebacken wird in einem möglichen weiteren Backvorgang. * 1000 um auf ms zu kommen 
+		Define_Timer.iSleepTimeMS -= 5 * 1000;																			// Dekrementiere die Zeit fï¿½r die die Waffel weiter gebacken wird in einem mï¿½glichen weiteren Backvorgang. * 1000 um auf ms zu kommen 
 	}
 	
 	public static void vWaitFor(int iWaitTime) {
 		/*
-		 * Warte um die übergebene Zeit
+		 * Warte um die ï¿½bergebene Zeit
 		 */
 		Delay.msDelay(iWaitTime);
 	}
